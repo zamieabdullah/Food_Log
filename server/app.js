0e754b321bd119c488bd4f567fb4766a8aa1b57f
zamieabdullah <zamieabdullah0@gmail.com>
2021-06-29 20:06:36 -0400

    Testing

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+// test
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -17,7 +19,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
     // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", 
+    res.setHeader("Access-Control-Allow-Headers",
         "Content-Type, Access-Control-Allow-Headers");
     next();
 });
