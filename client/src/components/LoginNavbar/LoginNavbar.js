2ed954d398fbc9b36b122045793bd020a566d6c1
zamieabdullah <zamieabdullah0@gmail.com>
2021-07-11 12:54:50 -0400

    redirect created for logging in and more steps taken for authentication

--- client/src/components/LoginNavbar/LoginNavbar.js
+++ client/src/components/LoginNavbar/LoginNavbar.js
@@ -1,18 +1,23 @@
 import React, { Fragment , useState } from 'react';
 import Axios from 'axios';
+import { Redirect } from 'react-router-dom';
 import './style.css'
 
 export default () => {
-    
+
     const [user, setUser] = useState({
         email : '',
         password : ''
     });
-    
+
+    const [valid, setValid] = useState({
+        valid : false
+    });
+
     const handleChange = (event) => {
         setUser({...user, [event.target.name] : event.target.value});
     };
-    
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -25,35 +30,36 @@ export default () => {
                     password : user.password
                 }
             });
-            
+
             setUser({
                 email : '',
                 password : ''
             });
-            
+
             localStorage.setItem('x-auth-token', resp.data.token);
             Axios.defaults.headers.common['x-auth-token'] = resp.data.token;
-            
-            console.log(resp.data.token);
-            
+
+            setValid({ valid : true });
+            localStorage.setItem('authenticated', valid.valid);
+
         } catch (err) {
             console.error(err);
         }
     }
-    
+
     return (
         <Fragment>
             <nav class='navbar justify-content-between'>
                 <h1>Food Log</h1>
                 <form className='form-inline' onSubmit={handleSubmit}>
                     <div className='form-group mr-2'>
-                        <input className='form-control' type='text' 
-                            name='email' value={user.email} 
-                            onChange={handleChange} 
+                        <input className='form-control' type='text'
+                            name='email' value={user.email}
+                            onChange={handleChange}
                             placeholder='Email Address' required/>
                     </div>
                     <div className='form-group mr-2'>
-                        <input className='form-control' type='text' 
+                        <input className='form-control' type='text'
                             name='password' value={user.password}
                             onChange={handleChange}
                             placeholder='Password' required/>
@@ -61,6 +67,7 @@ export default () => {
                     <button type='submit' className='btn btn-primary'>Submit</button>
                 </form>
             </nav>
+            {valid.valid ? <Redirect to='/' /> : null}
         </Fragment>
     )
 }
--- client/src/routes/AppRoute.js
+++ client/src/routes/AppRoute.js
@@ -1,10 +1,14 @@
-import React, { Fragment , useState } from 'react';
+import React, { Fragment , useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Axios from 'axios';
 import Home from '../pages/Home';
 import LoginSignUp from '../pages/LoginSignUp';
 
 export default () => {
+    useEffect(() => {
+
+    }, []);
+
     return (
         <Switch>
             <Route exact path='/'>
