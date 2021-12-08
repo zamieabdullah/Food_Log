04dc577e30fb00c4d81cb4bc2dd8cc8dc6a5a2ea
zamieabdullah <zamieabdullah0@gmail.com>
2021-12-08 16:02:09 -0500

    edits to FoodLog page

--- client/src/components/FoodLogForm/FoodLogForm.js
+++ client/src/components/FoodLogForm/FoodLogForm.js
@@ -6,31 +6,43 @@ export default () => {
         food_name : '',
         food_type : '',
         time_eaten : ''
-    })  
-    
+    })
+
     const handleChange = (event) => {
         setLog({...log, [event.target.name] : event.target.value});
     }
-    
+
+    const handleOption = (event) => {
+        setLog({ food_type : event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(log.food_name);
+    }
+
     return (
         <div className='container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='form-group mb-4'>
                     <label htmlFor='food_name' className='required'>Food Name</label>
-                    <input type='text' className='form-control' onChange={handleChange} required/>
+                    <input type='text' className='form-control'
+                    name='food_name' value={log.food_name} onChange={handleChange} required/>
                 </div>
                 <div className='form-group mb-4'>
                     <label htmlFor='food_type' className='required'>Food Type</label>
-                    <select className='form-control' required>
-                        <option>Breakfast</option>
-                        <option>Lunch</option>
-                        <option>Dinner</option>
-                        <option>Snack/Other</option>
+                    <select className='form-control' value={log.food_type} onChange={handleOption} required>
+                        <option value=''></option>
+                        <option value='Breakfast'>Breakfast</option>
+                        <option value='Lunch'>Lunch</option>
+                        <option value='Dinner'>Dinner</option>
+                        <option value='Snack/Other'>Snack/Other</option>
                     </select>
                 </div>
                 <div className='form-group mb-4'>
                     <label htmlFor='time_eaten' className='required'>Time Eaten</label>
-                    <input type='option' className='form-control' required/>
+                    <input type='option' className='form-control'
+                    name='time_eaten' value={log.time_eaten} onChange={handleChange} required/>
                 </div>
                 <div className='form-group mb-4'>
                     <button type="submit" className="btn btn-primary">Submit</button>
