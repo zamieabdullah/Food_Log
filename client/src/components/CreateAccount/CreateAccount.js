bb2d93c9f29d8cefbc83108a1503004b849e3c98
zamieabdullah <zamieabdullah0@gmail.com>
2021-12-26 22:00:03 -0500

    created hidden password for input

--- client/src/components/CreateAccount/CreateAccount.js
+++ client/src/components/CreateAccount/CreateAccount.js
@@ -7,7 +7,7 @@ import "../PopUp/style.css";
 
 export default () => {
     const history = useHistory();
-    
+
     const [account, setAccount] = useState({
         first_name: '',
         middle_name: '',
@@ -15,27 +15,27 @@ export default () => {
         email: '',
         password: ''
     });
-    
+
     const [seen, setSeen] = useState({
         seen: false
     });
-    
+
     const [message, setMessage] = useState({
         message: ''
     });
-    
+
     const togglePop = () => {
         setSeen({seen: !seen.seen});
     };
-    
+
     const addMessage = (newMessage) => {
         setMessage({message: newMessage});
     };
-    
+
     const handleChange = (event) => {
         setAccount({...account, [event.target.name]: event.target.value});
     };
-    
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -51,7 +51,7 @@ export default () => {
                     password: account.password
                 }
             });
-            
+
             setAccount({
                 first_name: '',
                 middle_name: '',
@@ -59,17 +59,28 @@ export default () => {
                 email: '',
                 password: ''
             });
-            
+
             localStorage.setItem('x-auth-token', resp.data.token);
             localStorage.setItem('authenticated', resp.data.auth);
-            
+
             history.push('/');
             window.location.reload();
         } catch (error) {
             togglePop();
             addMessage('Error has occurred when making account');
-        } 
+        }
     };
+
+    const showPass = (event) => {
+        event.preventDefault();
+        const pass = document.getElementById("psw");
+
+        if (pass.type === "password") {
+            pass.type = "text";
+        } else if (pass.type === "text") {
+            pass.type = "password";
+        }
+    }
 
     return (
         <Fragment>
@@ -77,45 +88,48 @@ export default () => {
             <form onSubmit={handleSubmit}>
                 <div className="form-group mb-4">
                     <label htmlFor="first_name" className="required">First Name</label>
-                    <input type="text" className="form-control" 
-                        placeholder="First Name" name="first_name" 
-                        value={account.first_name} 
+                    <input type="text" className="form-control"
+                        placeholder="First Name" name="first_name"
+                        value={account.first_name}
                         onChange={handleChange} required/>
                 </div>
                 <div className="form-group mb-4">
                     <label htmlFor="middle_name">Middle Name</label>
-                    <input type="text" className="form-control" 
-                        placeholder="Middle Name" name="middle_name" 
-                        value={account.middle_name} 
+                    <input type="text" className="form-control"
+                        placeholder="Middle Name" name="middle_name"
+                        value={account.middle_name}
                         onChange={handleChange}/>
                 </div>
                 <div className="form-group mb-4">
                     <label htmlFor="last_name" className="required">Last Name</label>
-                    <input type="text" className="form-control" 
+                    <input type="text" className="form-control"
                         placeholder="Last Name" name="last_name"
                         value={account.last_name}
                         onChange={handleChange} required/>
                 </div>
                 <div className="form-group mb-4">
                     <label htmlFor="email_address" className="required">Email Address</label>
-                    <input type="text" className="form-control" 
-                        placeholder="Email Address" name="email" 
+                    <input type="text" className="form-control"
+                        placeholder="Email Address" name="email"
                         value={account.email}
                         onChange={handleChange} required/>
                 </div>
                 <div className="form-group mb-4">
                     <label htmlFor="password" className="required">Password</label>
-                    <input type="text" className="form-control" 
-                        placeholder="Password" name="password" 
+                    <input type="password" className="form-control"
+                        id="psw" placeholder="Password" name="password"
                         value={account.password}
                         onChange={handleChange} required/>
+                </div>
+                <div className="form-group mb-4">
+                    <input type="checkbox" onClick={showPass}/> Show Password
                 </div>
                 <div className="form-group">
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
             </form>
             {seen.seen ? <PopUp toggle={togglePop} message={message.message}/> : null}
-                
+
         </Fragment>
     )
 }
